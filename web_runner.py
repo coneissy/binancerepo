0723d310ba3e439e9414c24a578e4540fcc6058e
coneissy <coneissyproworker@hotmail.com>
2026-09-14 21:04:04 +0300

    Switch Render engine entrypoint to futures triangle v4

--- web_runner.py
+++ web_runner.py
@@ -1,5 +1,5 @@
-"""Render entrypoint for Cryptoalpha Spot + Futures Cross v3."""
-from arbitrage_engine_v3 import main
+"""Render entrypoint for Cryptoalpha Spot + Futures Triangular v4."""
+from arbitrage_engine_v4 import main
 
 if __name__ == "__main__":
     main()
