9fd5028fb0b4603235209535f13beedcf54f8ae4
coneissy <coneissyproworker@hotmail.com>
2026-09-14 21:46:29 +0300

    Switch Render entrypoint to WebSocket-stable v6

--- web_runner.py
+++ web_runner.py
@@ -1,5 +1,5 @@
-"""Render entrypoint for Cryptoalpha robust paper engine v5."""
-from arbitrage_engine_v5 import main
+"""Render entrypoint for Cryptoalpha robust paper engine v6."""
+from arbitrage_engine_v6 import main
 
 if __name__ == "__main__":
     main()
