81a55f590cd33acd836a7d058cbd6c9db7b59ae1
coneissy <coneissyproworker@hotmail.com>
2026-09-16 23:16:36 +0300

    Point Render entrypoint to canonical Dragon core

--- web_runner.py
+++ web_runner.py
@@ -1,5 +1,5 @@
-"""Render entrypoint for Cryptoalpha v7 gated live-capable engine."""
+"""Render entrypoint for Dragon v7 canonical engine."""
 import arbitrage_engine_v7 as v7
 
 if __name__ == "__main__":
-    v7.v6.main()
+    v7.core.main()
